82ff559c96cf4998d3832ff47545fc8136f78e1e
arunavn <arunavnarayan@gmail.com>
2023-07-16 14:26:58 +0530

    config updates

--- cdk-app/config/app_config.py
+++ cdk-app/config/app_config.py
@@ -8,12 +8,13 @@ class Config:
             self.config_dict = json.load(f)
         self.deploy_env =  os.environ['DEPLOY_ENV']
         self.env_dict = self.config_dict.get(self.deploy_env, None)
+        self.isdr = os.environ['REGION'] == self.env_dict['drRegion']
     
     def stack_name(self) -> str:
         return self.config_dict.get('stackName', '')
     
     def account_id(self) -> str:
-        return self.env_dict.get('accountId', '')
+        return os.environ['ACCOUNT']
     
     def deploy_region(self) -> str:
         return self.env_dict['primaryRegion'] 
